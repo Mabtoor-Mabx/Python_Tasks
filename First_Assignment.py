5c7ef2b5d9b98b67cdd2278bec7b85046ecd946f
Mabtoor-Mabx <mabtoorulshafiq381@gmail.com>
2020-11-04 14:22:18 +0500

    First Practice Set

---
+++ First_Assignment.py
@@ -0,0 +1,23 @@
+# Problem 1
+
+print(''' Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky''')
+
+# Problem 2
+
+
+# Problem No  4
+
+
+import os
+print(os.listdir())
+
+# Problem No 3
+
+from playsound import playsound
+playsound('play.mp3')
+# Problem 5
+
+# ['.vscode', 'First_Assignment.py', 'play.mp3', 'Test.py']
