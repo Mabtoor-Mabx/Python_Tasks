4fe59a8607d113882df3f47299d7a9c9d40f78d2
Mabtoor-Mabx <mabtoorulshafiq381@gmail.com>
2020-11-05 11:41:57 +0500

    Variable Assignment Done

---
+++ Assignment_2.py
@@ -0,0 +1,30 @@
+# Problem No  1
+
+# a= 23
+# b= 42
+# c = a+b
+# print( "The Sum Of Two Values is ",c)
+
+# Problem No 2
+
+# a = 312
+# b= 213
+# c= a%b
+# print("The Remainder Of a&b is" , c)
+
+# Problem No 3
+
+# name = input("Enter Your Name")
+# print(type(name))
+
+# Problem 4
+
+
+# a=34
+# b=80
+# print(a>b)
+
+# Problem 5
+
+number = input("Enter A Number")
+print("The Square of a Number is", int(number) * int(number) )
---
+++ Variables.py
@@ -0,0 +1,10 @@
+# Just A Basic Program Of Variable
+
+name = input("Enter Your Name")
+age = input("Enter Your Age")
+obt_marks = 454
+total_marks = 500
+percent = (obt_marks/total_marks)*100
+print(int(age) + 3)
+
+print("Dear", name , "Age" , age , "Your Percentage is" , percent)
