2d5fb81b346dc75e3f33affb1b0262263e621333
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2020-11-24 16:58:43 +0500

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,34 @@
+# no = 50
+# i = 0
+
+# while(i<=no):
+#     print(i)
+#     i= i+1
+
+
+
+
+liste = ["Banana" , "Orange" , "Apple" , "Peach"]
+
+for i in liste:
+    print(i)
+
+
+# i=0
+# while i<len(liste):
+#     print(i)
+#     i=i+1
+
+
+
+for o in range(1,8):
+    print(o)
+
+
+
+
+for loope in range(1,10,2):
+    print(loope)
+
+
+
