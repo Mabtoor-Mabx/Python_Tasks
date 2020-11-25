0ba49b4819234bf41ad486f61a34fe0e41da6b4e
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2020-11-25 16:24:50 +0500

    Add files via upload

---
+++ Assignment_7.py
@@ -0,0 +1,90 @@
+## Problem 1
+# no = int(input("Enter a Number : \n"))
+# for i in range(1,11):
+#     print(f" {no} X {i} = {no*i}")
+
+
+
+# Problem 2
+
+lisye= ["Shubham", "Sooraj", "Divya" " Harry"]
+for i in lisye:
+    if i.startswith("S"):
+        print(f"Greetings {i}")
+
+
+
+# Problem 3
+
+
+# no = int(input("Enter A Number \n"))
+
+# i= 1
+# while i<=10:
+#     print(f" {no} X {i} = {no *i} ")
+#     i = i+1
+
+
+
+# Problem 4
+
+# no = int(input("Enter A  Number \n"))
+# prime = True
+
+# for i in range(2,no):
+#     if no%2==0:
+#         prime = False
+
+# if prime:
+#      print("This Is a Prime Number")
+# else:
+#      print("This Is Not A Prime Number")
+
+
+
+# Problem 6
+# no = int(input("Enter A Number"))
+# fac = 1
+# for i  in range(1,no+1):
+#     fac = fac * i
+# print(fac)
+
+
+
+# Problem 7
+
+# n = 3
+# for i in range(3): 
+#     print(" " * (n-i-1), end="")
+#     print("*" * (2*i+1), end="")
+#     print(" " * (n-i-1))
+
+
+# no = 3
+# for i  in range(3):
+#     print(" " * (no-i-1), end="")
+#     print("*" * (2*i+1), end="")
+#     print(" " * (no-i-1)) 
+
+
+
+# Problem 8
+
+
+# no = 4
+# for i in range(4):
+#     print("*" * (i+1))
+
+
+# problem 10
+
+# no = int(input("Enter A Number"))
+
+# for i in reversed(range(1,11)):
+#     print(f" {no} X {i} = {no*i} ")
+
+
+
+
+
+
