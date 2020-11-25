cefc1d6f3a979cfa2602bda081e44e75f1621561
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2020-11-25 21:07:32 +0500

    Add files via upload
    Done

---
+++ SevenAssignment.py
@@ -0,0 +1,85 @@
+#  Problem 1
+
+# no = 4
+
+# for i in range(1,11):
+#     print(f" {no} X {i} = {no*i}")
+
+
+# Problem 2
+
+
+# l = ["Harry", "Shubham", "Sohan", "Divya"]
+ 
+# for i in l:
+#     if i.startswith("S"):
+#         print("Greeting " + i)
+
+
+# Problem 3
+
+
+# no = 5
+# i = 1
+
+# while i<11:
+#     print(f" {no} X  {i} = {no*i} ")
+#     i = i+1
+
+
+# Problem 4
+
+
+# no = int(input("Enter Your Number \n"))
+# prime = True
+# for i in range(2,no):
+#     if no%2==0:
+#         prime = False
+
+# if prime:
+#     print("This Is A Prime Number")
+# else:
+#      print("This Is Not A Prime Number")
+
+
+
+
+
+# Problem 5
+
+# no = int(input("Enter A Number \n "))
+
+# sum = 0
+# i = 1
+# while i<no:
+#     sum = sum + i
+#     i= i+1
+
+# print(f"The Sum Of Value Is : {sum} " )    
+
+
+
+
+# Problem 6
+
+
+
+# no = int(input("Enter A Number"))
+
+# fac = 1
+
+# for i in range(1, no+1):
+#     fac = fac *i
+
+# print(fac)   
+
+
+
+
+# Problem 10
+
+
+# no = int(input("Enter A Number \n"))
+
+# for i in reversed(range(1,11)):
+#     print(f" {no} X {i} = {no * i} ")
