42a46b9d0bd230e095a5c1926fcb014f110f6ee3
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2020-12-28 21:35:33 +0500

    Add files via upload
    Constructor and Self Method in Python

---
+++ Constructor.py
@@ -0,0 +1,20 @@
+#  STATIC METHOD 
+
+
+# class Employee:
+#     @staticmethod
+#     def Func1():
+#         print("HelloWorld!!!")
+# Mabx = Employee()
+# Mabx.Func1()        
+
+
+
+#  CONSTRUCTOR
+
+class Employee:
+    def __init__(self):
+        print("Hello Beautiful!")
+
+
+Harry = Employee()
